7621e09693e21670815b8fc877d5a48bd5d1fdb5
N54Karim <karimelsayed713@gmail.com>
2024-12-09 19:43:19 -0800

    colorpicker.py

--- colorpicker.py
+++ colorpicker.py
@@ -4,23 +4,19 @@ import csv
 
 # Function to pick color and update label
 def pick_color():
-    # Open the color picker dialog
+    # Open dialog
     color_code = colorchooser.askcolor(title="Choose a color")[1]
     
-    if color_code:  # Only proceed if the user selects a color
-        # Update the label with the selected color
+    if color_code:
         color_label.config(text=color_code, bg=color_code)
-        # Save the selected color
         save_color(color_code)
 
-# Function to save the color to a CSV file
+# Save the color to a CSV file
 def save_color(color_code):
-    # Save the color to a CSV file
     with open('colors.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([color_code])
 
-# Set up the root window
 root = tk.Tk()
 root.title("Color Picker App")
 
@@ -32,6 +28,5 @@ pick_button.pack(pady=20)
 color_label = tk.Label(root, text="Selected Color", width=30, height=5)
 color_label.pack(pady=20)
 
-# Run the application
 root.mainloop()
 
